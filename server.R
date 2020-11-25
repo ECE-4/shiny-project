fd85ae4aff400601420128399d40400689291001
NeilSCGH <neil.segard@gmail.com>
2020-11-25 20:21:26 +0100

    removing useless code

--- server.R
+++ server.R
@@ -2,88 +2,66 @@ library(shiny)
 library(ggplot2)
 shinyServer(function(input, output) {
   ## Tab 1
-  output$text <- renderPrint({ input$features })
-    
-  output$plotFrance <- renderPlot(
-    if ("1" %in% {
-      input$city
-    }) {
-      ggplot(bordeaux1, aes_string(input$features)) +
-        geom_histogram(
-          breaks = seq(min(input$sliderX[1]), max(input$sliderX[2]), by = 1),
-          col = "purple",
-          fill = "purple",
-          alpha = .2
-        ) +
-        ylim(input$sliderY[1], input$sliderY[2]) +
-        ggtitle("Bordeaux")
-    }
-  )
-  
-  output$plotSpain <- renderPlot(
-    if ("2" %in% {
-      input$city
-    }) {
-      
-    }
-  )
-  
-  output$plotItaly <- renderPlot(
-    if ("3" %in% {
-      input$city
-    }) {
-      
-    }
-  )
+  output$text <- renderPrint({
+    input$features
+  })
 
-  output$plotGermany <- renderPlot(
-    if ("4" %in% {
-      input$city
-    }) {
-      ggplot(berlin2, aes_string(input$features)) +
-        geom_histogram(
-          breaks = seq(min(input$sliderX[1]), max(input$sliderX[2]), by = 1),
-          col = "yellow",
-          fill = "yellow",
-          alpha = .2
-        ) +
-        ylim(input$sliderY[1], input$sliderY[2]) +
-        ggtitle("Berlin")
-    }
-  )
+  output$plotFrance <- renderPlot({
+    ggplot(bordeaux1, aes_string(input$features)) +
+      geom_histogram(
+        breaks = seq(min(input$sliderX[1]), max(input$sliderX[2]), by = 1),
+        col = "purple",
+        fill = "purple",
+        alpha = .2
+      ) +
+      ylim(input$sliderY[1], input$sliderY[2]) +
+      ggtitle("Bordeaux")
+  })
 
-  output$plotNetherlands <- renderPlot(
-    if ("5" %in% {
-      input$city
-    }) {
-      ggplot(amsterdam1, aes_string(input$features)) +
-        geom_histogram(
-          breaks = seq(min(input$sliderX[1]), max(input$sliderX[2]), by = 1),
-          col = "red",
-          fill = "red",
-          alpha = .2
-        ) +
-        ylim(input$sliderY[1], input$sliderY[2]) +
-        ggtitle("Amsterdam")
-    }
-  )
+  output$plotSpain <- renderPlot({
 
-  output$plotBelgium <- renderPlot(
-    if ("6" %in% {
-      input$city
-    }) {
-      ggplot(brussels1, aes_string(input$features)) +
-        geom_histogram(
-          breaks = seq(min(input$sliderX[1]), max(input$sliderX[2]), by = 1),
-          col = "green",
-          fill = "green",
-          alpha = .2
-        ) +
-        ylim(input$sliderY[1], input$sliderY[2]) +
-        ggtitle("Brussels")
-    }
-  )
-  
-  
+  })
+
+  output$plotItaly <- renderPlot({
+
+  })
+
+  output$plotGermany <- renderPlot({
+    ggplot(berlin2, aes_string(input$features)) +
+      geom_histogram(
+        breaks = seq(min(input$sliderX[1]), max(input$sliderX[2]), by = 1),
+        col = "yellow",
+        fill = "yellow",
+        alpha = .2
+      ) +
+      ylim(input$sliderY[1], input$sliderY[2]) +
+      ggtitle("Berlin")
+  })
+
+  output$plotNetherlands <- renderPlot({
+    ggplot(amsterdam1, aes_string(input$features)) +
+      geom_histogram(
+        breaks = seq(min(input$sliderX[1]), max(input$sliderX[2]), by = 1),
+        col = "red",
+        fill = "red",
+        alpha = .2
+      ) +
+      ylim(input$sliderY[1], input$sliderY[2]) +
+      ggtitle("Amsterdam")
+  })
+
+  output$plotBelgium <- renderPlot({
+    ggplot(brussels1, aes_string(input$features)) +
+      geom_histogram(
+        breaks = seq(min(input$sliderX[1]), max(input$sliderX[2]), by = 1),
+        col = "green",
+        fill = "green",
+        alpha = .2
+      ) +
+      ylim(input$sliderY[1], input$sliderY[2]) +
+      ggtitle("Brussels")
+  })
+
+
   ## Tab 2
 })
